0e6dbf174a6781ed76d13d84c9e0ac4fa57ac3fb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-12-08 23:54:04 +0700

    New FFT wrappers, multicolumn R_to_G! is still used

--- src/cu_wrappers_fft.jl
+++ src/cu_wrappers_fft.jl
@@ -1,145 +1,96 @@
 import PWDFT: G_to_R, G_to_R!, R_to_G, R_to_G!
+import GPUArrays: AbstractGPUArray
 
-function G_to_R( pw::CuPWGrid, fG::CuArray{ComplexF64,1} )
-    Ns = pw.Ns
-    Npoints = prod(Ns)
-    plan = pw.planbw
-    out = reshape( plan*reshape(fG,Ns), Npoints )
-    return out
-end
-
-# without reshape
-function G_to_R( pw::CuPWGrid, fG::CuArray{ComplexF64,3} )
-    return pw.planbw*fG
-end
-
-function G_to_R( pw::CuPWGrid, fG::CuArray{ComplexF64,2} )
-    Ns = pw.Ns
-    Npoints = prod(Ns)
-    plan = pw.planbw
-    out = CUDA.zeros( ComplexF64, size(fG) )
-    for ic = 1:size(fG,2)
-        out[:,ic] = reshape( plan*reshape(fG[:,ic],Ns), Npoints )
-    end
-    return out
-end
-
-function G_to_R!( pw::CuPWGrid, fG::CuArray{ComplexF64,1} )
-    Ns = pw.Ns
-    Npoints = prod(Ns)
-    plan = pw.planbw
-    fG[:] = reshape( plan*reshape(fG[:],Ns), Npoints )
+#
+# In-place version, input 3d data as 3d array
+#
+function G_to_R!( pw, fG::CuArray{ComplexF64,3} )
+    pw.planbw*fG
     return
 end
 
-function G_to_R!( pw::CuPWGrid, fG::CuArray{ComplexF64,2} )
-    Ns = pw.Ns
-    Npoints = prod(Ns)
-    plan = pw.planbw
-    for ic = 1:size(fG,2)
-        fG[:,ic] = reshape( plan*reshape(fG[:,ic],Ns), Npoints )
-    end
+function R_to_G!( pw, fR::CuArray{ComplexF64,3} )
+    pw.planfw*fR
     return
 end
 
-function R_to_G( pw::CuPWGrid, fR::CuArray{ComplexF64,1} )
-    Ns = pw.Ns
-    Npoints = prod(Ns)
-    plan = pw.planfw
-    out = reshape( plan*reshape(fR,Ns), Npoints )
-    return out
+#
+# Return new array, input 3d data as 3d array
+#
+function G_to_R( pw, fG::CuArray{ComplexF64,3} )
+    ff = copy(fG)
+    pw.planbw*ff
+    return ff
 end
 
-function R_to_G( pw::CuPWGrid, fR::CuArray{ComplexF64,3} )
-    return pw.planfw*fR
+function R_to_G( pw, fR::CuArray{ComplexF64,3} )
+    ff = copy(fR)
+    pw.planfw*ff
+    return ff
 end
 
+
+#
+# In-place version, input 3d data as column vector
+#
+function G_to_R!( pw, fG::AbstractGPUArray{ComplexF64,1} )
+    ff = reshape(fG, pw.Ns)
+    pw.planbw*ff
+    return
+end
+
+function R_to_G!( pw, fR::AbstractGPUArray{ComplexF64,1} )
+    ff = reshape(fR, pw.Ns)
+    pw.planfw*ff
+    return
+end
+
+
+#
+# Return a new array
+#
+function G_to_R( pw, fG::AbstractGPUArray{ComplexF64,1} )
+    ff = copy(fG)
+    ff = reshape(ff, pw.Ns)
+    pw.planbw*ff
+    return reshape(ff, prod(pw.Ns))
+end
+
+function R_to_G( pw, fR::AbstractGPUArray{ComplexF64,1} )
+    ff = copy(fR)
+    ff = reshape(fR, pw.Ns)
+    pw.planfw*ff
+    return reshape(ff, prod(pw.Ns))
+end
+
+
+#
 # used in Poisson solver
-function R_to_G( pw::CuPWGrid, fR_::CuArray{Float64,1} )
-    Ns = pw.Ns
-    Npoints = prod(Ns)
-    plan = pw.planfw
-    fR = convert(CuArray{ComplexF64,1},fR_)
-    out = reshape( plan*reshape(fR,Ns), Npoints )
-    return out
-end
-
-function R_to_G( pw::CuPWGrid, fR::CuArray{ComplexF64,2} )
-    Ns = pw.Ns
-    plan = pw.planfw
-    Npoints = prod(Ns)
-    Ncol = size(fR,2)
-    out = CUDA.zeros(ComplexF64, size(fR))
-    for ic = 1:Ncol
-        out[:,ic] = reshape( plan*reshape(fR[:,ic],Ns), Npoints )
-    end
-    return out
-end
-
-function R_to_G!( pw::CuPWGrid, fR::CuArray{ComplexF64,2} )
-    Ns = pw.Ns
-    plan = pw.planfw
-    Npoints = prod(Ns)
-    Ncol = size(fR,2)
-    for ic = 1:Ncol
-        fR[:,ic] = reshape( plan*reshape(fR[:,ic],Ns), Npoints )
-    end
-    return
-end
-
-function R_to_G!( pw::CuPWGrid, fR::CuArray{ComplexF64,1} )
-    Ns = pw.Ns
-    plan = pw.planfw
-    Npoints = prod(Ns)
-    fR[:] = reshape( plan*reshape(fR[:],Ns), Npoints )
-    return
+#
+function R_to_G( pw, fR_::AbstractGPUArray{Float64,1} )
+    fR = convert(CuArray{ComplexF64,1}, fR_) # This will make a copy
+    ff = reshape(fR, pw.Ns)
+    pw.planfw*ff
+    return reshape(ff, prod(pw.Ns))
 end
 
 
 #
-# Using fft and ifft directly
+# Used in calc_rhoe
 #
-
-function G_to_R( Ns::Tuple{Int64,Int64,Int64}, fG::CuArray{ComplexF64,1} )
-    out = reshape( ifft( reshape(fG,Ns) ),size(fG) )
-end
-
-# without reshape
-function G_to_R( Ns::Tuple{Int64,Int64,Int64}, fG::CuArray{ComplexF64,3} )
-    out = ifft(fG)
-end
-
-# multicolumn
-function G_to_R( Ns::Tuple{Int64,Int64,Int64}, fG::CuArray{ComplexF64,2} )
-    Npoints = prod(Ns)
-    out = CUDA.zeros(ComplexF64, size(fG))
-    for ic = 1:size(fG)[2]
-        out[:,ic] = reshape( ifft( reshape(fG[:,ic],Ns) ), Npoints )
+function G_to_R!( pw, fG::CuMatrix{ComplexF64} )
+    for i in 1:size(fG,2)
+        @views ff = reshape(fG[:,i], pw.Ns)
+        pw.planbw*ff
     end
-    return out
+    return
 end
 
-function R_to_G( Ns::Tuple{Int64,Int64,Int64}, fR::CuArray{ComplexF64,1} )
-    out = reshape( fft( reshape(fR,Ns) ), size(fR) )
-end
-
-# without reshape
-function R_to_G( Ns::Tuple{Int64,Int64,Int64}, fR::CuArray{ComplexF64,3} )
-    out = fft(fR)
-end
-
-# In case we forget to convert the input, we convert it in this version
-function R_to_G( Ns::Tuple{Int64,Int64,Int64}, fR_::CuArray{Float64,1} )
-    fR = convert(CuArray{ComplexF64,1}, fR_)
-    out = reshape( fft( reshape(fR,Ns) ), size(fR) )
-end
-    
-function R_to_G( Ns::Tuple{Int64,Int64,Int64}, fR::CuArray{ComplexF64,2} )
-    Npoints = prod(Ns)
-    Ncol = size(fR)[2]
-    out = CUDA.zeros( ComplexF64, size(fR) )
-    for ic = 1:Ncol
-        out[:,ic] = reshape( fft( reshape(fR[:,ic],Ns) ), Npoints )
+# Used op_V_loc, multicolumn
+function R_to_G!( pw, fR::CuArray{ComplexF64,2} )
+    for i in 1:size(fR,2)
+        @views ff = reshape(fR[:,i], pw.Ns)
+        pw.planfw*ff
     end
-    return out
+    return
 end
