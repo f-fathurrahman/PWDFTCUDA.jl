cd5526a30f15571862b60a4a224df395987c24a2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-12-09 09:42:29 +0700

    Update to ComplexF64, using inplace FFT

--- test/test_fft_3d_v2.jl
+++ test/test_fft_3d_v2.jl
@@ -1,4 +1,3 @@
-using BenchmarkTools
 using Printf
 using CUDA
 using PWDFT
@@ -6,36 +5,24 @@ using PWDFTCUDA
 
 function main_CPU(; ecutwfc=15.0)
     pw = PWGrid(ecutwfc, gen_lattice_sc(20.0))
-
     Npoints = prod(pw.Ns)
-    Rhoe = rand(Float64,Npoints)
-
-    RhoeG = R_to_G( pw, Rhoe )
-
+    Rhoe = rand(ComplexF64,Npoints)
     @printf("%8d %8d %8d CPU: ", pw.gvec.Ng, pw.Ns[1], Npoints)
-    
-    #@btime R_to_G( $pw, $Rhoe )
-    @btime R_to_G( $pw.Ns, $Rhoe )
+    R_to_G!(pw, Rhoe) # warm up
+    @time R_to_G!(pw, Rhoe)
 end
 
 function main_GPU(; ecutwfc=15.0)
     pw = CuPWGrid(ecutwfc, gen_lattice_sc(20.0))
-
     Npoints = prod(pw.Ns)
-    Rhoe = CuArray(rand(Float64,Npoints))
-
-    RhoeG = R_to_G( pw, Rhoe )
-    
+    Rhoe = CuArray(rand(ComplexF64,Npoints))
     @printf("%8d %8d %8d GPU: ", pw.gvec.Ng, pw.Ns[1], Npoints)
-
-    @btime begin
-        #CUDA.@sync R_to_G( $pw, $Rhoe )
-        CUDA.@sync R_to_G( $pw.Ns, $Rhoe )
-    end
+    R_to_G!(pw, Rhoe) # warm up
+    @time CUDA.@sync R_to_G!(pw, Rhoe)
 end
 
 for ecut in 30.0:5.0:60.0
     println("ecut = ", ecut)
-    #main_CPU(ecutwfc=ecut)
+    main_CPU(ecutwfc=ecut)
     main_GPU(ecutwfc=ecut)
 end
